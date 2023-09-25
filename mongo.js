37bc36c66856be13c88ba10d3544585c4c4d76ba
Gaurav <Gaurav123@gmail.com>
2023-09-25 15:06:38 +0530

    commit

--- mongo.js
+++ mongo.js
@@ -1,21 +1,36 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
 
 require("dotenv").config();
 
-const {
-    MONGO_URL,
-    MONGO_USERNAME,
-    MONGO_PASSWORD
-} = process.env;
+const { MONGO_URL, MONGO_USERNAME, MONGO_PASSWORD } = process.env;
 
-
-//Testing server
-mongoose.connect(MONGO_URL, { 
-    auth: { 
-        user: MONGO_USERNAME,   
-        password: MONGO_PASSWORD 
+if(MONGO_USERNAME != "") {
+  mongoose.connect(MONGO_URL, {
+    auth: {
+        user: MONGO_USERNAME,
+        password: MONGO_PASSWORD
     },
     opt: { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false }
+  });
+}
+else {
+  mongoose.connect(MONGO_URL, {
+    opt: {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    },
+  });
+}
+
+const conSuccess = mongoose.connection;
+conSuccess.once("open", (_) => {
+  console.log("Database connected:");
+});
+
+conSuccess.on("error", (err) => {
+  console.error("connection error:", err);
 });
